5e761c38a5d9a28a75c436da108eedb2ff2a4953
Valencia <vlrichard00@gmail.com>
2016-10-18 21:30:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/astoldbyvee/homework3_part1/PartOne.java
@@ -0,0 +1,13 @@
+package com.example.astoldbyvee.homework3_part1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part_one);
+    }
+}
